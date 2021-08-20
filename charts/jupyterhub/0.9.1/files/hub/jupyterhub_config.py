8ee1720375424ca48de826a4785e4968c9e9555f
Taylor Johnston <tayl1055@purdue.edu>
2021-08-20 15:29:58 -0700

    added code to catch empty allowedGroups

--- charts/jupyterhub/0.9.1/files/hub/jupyterhub_config.py
+++ charts/jupyterhub/0.9.1/files/hub/jupyterhub_config.py
@@ -372,10 +372,12 @@ elif auth_type == 'ldap':
     
     groups = get_config('auth.ldap.allowedGroups')
     allowed_groups = groups.split(',')
+    if groups == ""
+        allowed_groups = []
     if groups is not None:
         for i in range(len(allowed_groups)):
             allowed_groups[i] = allowed_groups[i].strip()
-            allowed_groups[i] = 'cn='+allowed_groups[i]+',ou=groups,dc=rcac,dc=purdue,dc=edu'		
+            allowed_groups[i] = 'cn='+allowed_groups[i]+',ou=groups,dc=rcac,dc=purdue,dc=edu'	
     else:
         allowed_groups = []
     setattr(c.LDAPAuthenticator, 'allowed_groups', allowed_groups)
